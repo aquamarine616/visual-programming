a02b5f3510500d01fb0ef9081d29f7830ec0671f
Rudish Timofei <lolikvolik2017@gmail.com>
2026-05-11 21:39:34 +0700

    delete spreeadsheet-app

--- react/spreadsheet-app/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { useAppSelector } from './store/hooks'
-import Dashboard from './components/Dashboard'
-import Editor from './components/Editor'
-
-export default function App() {
-  const activeId = useAppSelector((s: { documents: { activeId: any } }) => s.documents.activeId)
-  return activeId ? <Editor /> : <Dashboard />
-}
